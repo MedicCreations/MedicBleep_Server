1c02e1b60b8db68efa1f289f49287cf969491197
Ken Yasue <ken.yasue@clover-studio.com>
2014-09-06 09:13:29 +0200

    remove init

--- Client/js/app/libs/init.php
+++
@@ -1,9 +0,0 @@
-// config
-var API_URL = 'http://localhost:8080/Server/wwwroot/v1';
-var WEB_ROOT = 'http://localhost:8080/Client'
-var AES_PASSWORD = 'jFglBLpOJQ4RLlVTl5EulWS2NLrTgHzB';
-
-var POOLING_INTERVAL = 500; // ms
-var HOLDER = "#spika_window";
-var WINDOW_MODE = true;
-var THREE_COLMODE_WIDTH = 640;
